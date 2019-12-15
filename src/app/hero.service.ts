294c94d3563edaa7e5bc7c8c3fd7a512da88bb9f
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2019-12-15 08:22:07 -0800

    Add in memor data service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
-
-import { Hero } from './hero';
 import { Observable, of } from 'rxjs'; 
-
-import { HEROES } from './mock-heroes';
+import { map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Hero } from './hero';
 import { MessageService } from './message.service';
 
 @Injectable({
@@ -11,16 +10,24 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    //return of(HEROES.find(hero => hero.id === id));
+    return this.getHeroes().pipe(map(heroes => heroes.find(hero => hero.id === id)));
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 
 }
