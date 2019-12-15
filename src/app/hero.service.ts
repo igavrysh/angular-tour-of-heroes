461dd9aebdd2daea7f3d8d18e26d7826246d1c02
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2019-12-14 20:17:24 -0800

    Add messages and messages components

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 
 import { Hero } from './hero';
+import { Observable, of } from 'rxjs'; 
+
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
   }
 
 }
