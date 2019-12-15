7489e73df3036f31f6065558019f9522c6b5f379
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2019-12-15 10:28:16 -0800

    Add new hero feature

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,7 +9,7 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  
+
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
@@ -19,6 +19,17 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
   }
 
 }
